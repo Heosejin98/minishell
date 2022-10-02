b1cb5acd9c81f339a406f73de0e5f9b588074e77
Heosejin98 <sejin3319@naver.com>
2022-10-02 12:55:23 +0900

    add Decoder header

---
+++ include/decoder.h
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   decoder.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seheo <seheo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/02 12:53:39 by seheo             #+#    #+#             */
+/*   Updated: 2022/10/02 12:53:44 by seheo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DECODER_H
+# define DECODER_H
+
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <stdbool.h>
+
+typedef enume_parser_tok_type
+{
+	std = 0,
+	subshell = 1,
+	and = 2,
+	or = 3,
+}	t_par_tok_type;
+
+typedef enum	e_parser_redirection_type
+{
+	is_pipe = 0,
+	is_in = 1,
+	is_in_heredoc = 2,
+	is_out = 3,
+	is_out_append = 4,
+}	t_redir_type;
+
+typedef struct s_parser_tok
+{
+	t_par_tok_type	type;
+	t_redir_type	redir_type[5];
+	char			**cmd;
+	size_t			cmd_size;
+	char			**in;
+	size_t			in_size;
+	char			**out;
+	size_t			out_size;
+}	t_par_tok;
+
+typedef enum e_iterator
+{
+	lex = 0,
+	par = 1,
+	cmd = 2,
+	in = 3,
+	out = 4,
+}	t_iter;
+
+typedef struct s_expander_tokens
+{
+	char			**cmd;
+	int				in;
+	int				out;
+	bool			is_pipe;
+}	t_exp_tok;
+
+#endif
